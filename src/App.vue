845a3243539d0c8db2f62e43d6eaa5d3b1f84685
Michele Abategiovanni <112349713+MicheleAbategiovanni@users.noreply.github.com>
2023-03-06 00:03:36 +0100

    init project

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <TheHeader />
+
+  <h2>Ciao</h2>
+</template>
+
+
+<script>
+import TheHeader from "./components/TheHeader.vue";
+
+export default {
+  components: { TheHeader },
+}
+</script>
+
+
+<style lang="scss">
+@use "./styles/general.scss";
+</style>
